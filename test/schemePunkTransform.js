209dec19eb369b07d0b23015d00790e54473e96a
David Diers <david.diers@fourkitchens.com>
2016-11-13 12:42:28 -0600

    Added super checks on mixins and implementing classes.

--- test/schemePunkTransform.js
+++ test/schemePunkTransform.js
@@ -4,7 +4,10 @@ const SchemePunkTransform = require('../lib/transform/schemePunkTransform');
 
 const schemePunkTransform = new SchemePunkTransform();
 
+// console.log(schemePunkTransform.constructor.name);
+
 module.exports = {
+
   schemePunkTransform: (test) => {
     const value = 'test';
     schemePunkTransform.transform(value);
