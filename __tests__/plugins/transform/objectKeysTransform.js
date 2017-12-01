a4896cd143648b23152daefce59fa733e4497b5a
David Diers <david.diers@fourkitchens.com>
2017-11-30 21:21:22 -0600

    fix(jest): Added objectKeysTransform.

---
+++ __tests__/plugins/transform/objectKeysTransform.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const ObjectKeysTransform = require('../../../lib/plugins/transform/objectKeysTransform');
+
+class BaseXform {
+  constructor(options) {
+    this.options = options;
+  }
+
+  transform(value) { // eslint-disable-line class-methods-use-this
+    return value;
+  }
+}
+const objectKeysTransform = new (ObjectKeysTransform(BaseXform))();
+
+let value;
+
+describe('Object keys', () => {
+  test('objectKeysTransformNoSuper.', () => {
+    value = {
+      test1: 'thing',
+      test2: 'thing2',
+      test3: 'thing3'
+    };
+    expect(objectKeysTransform.transform(value)).toEqual(['test1', 'test2', 'test3']);
+  });
+});
