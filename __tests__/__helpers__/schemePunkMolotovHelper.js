dd96068c3610513e9f91a4b509a157470dfe0176
David Diers <david.diers@gmail.com>
2018-03-06 13:00:23 -0600

    fix(bugs): Fix async debugging and other. (#109)
    * fix(debug): Improved debugging for plugin assembly.
    
    * fix(lint): One last max len.
    
    * fix(holdovers): get holdovers should be a promise.
    
    * fix(async): Async getHoldovers fixes.
    
    * fix(lint): Linting error fixes.
    
    * fix(tests): Schemepunk coverage for base classes.
    
    * fix(lint): Fixes destination.

---
+++ __tests__/__helpers__/schemePunkMolotovHelper.js
@@ -0,0 +1,67 @@
+'use strict';
+
+
+const molotovConfig = require('./molotov.json');
+// require the directory with all of your supers classes.
+// THe directory will be organized with directories representing
+// super name spaces
+// and those directories will hold your supers classes.
+//  supersDirectory
+//  |-- exampleSuperNameSpace.js
+const supers = require('./supers');
+
+// require the directory with all of your mixins.
+// THe directory will be organized with directories representing
+// super name spaces
+// and those directories will hold your mixins
+//  plugins
+//  |-- exampleSupersNameSpace
+//  |-- | -- mixinOne.js
+//  |-- | -- mixinTwo.js
+const mixins = require('./plugins');
+
+
+// Require the molotov file from the molotov module.
+// You will be extending this class.
+const Molotov = require('molotov/lib/molotov');
+
+const molotovNameSpace = 'schemePunk';
+
+// By extending the molotov class you use the common molotov
+// interface and provide your super and requires at require time.
+// ensuring require caching.
+
+// Also, by extending the molotov class and indicating the path
+// in your .molotov.json file you allow other module writers
+// to use or extend your plugins with their cocktail classes.
+
+const SchemePunkMolotov = class extends Molotov {
+  constructor(overrides, cocktailClasses) {
+    super(molotovConfig, molotovNameSpace, supers, mixins, overrides, cocktailClasses);
+  }
+  getMolotov() {
+    return {
+      resolve: () => {
+        return {
+          source: {},
+          transform: {},
+          destination: {},
+        }
+      }
+    }
+  }
+};
+
+/**
+ * SchemePunkMolotov Factory.
+ * @param {Object} configObject
+ *   Options for the schemePunkMolotov.
+ * @param {Object} configObject.config
+ *   Any dynamic molotov configuration to pass along.
+ * @param {Array} configObject.cocktailClasses
+ *   An array of cocktail classes that modules implementing schemePunk may
+ *   use to override plugins and supers.
+ */
+module.exports = function schemePunkMolotov(configObject = {overrides: {}, cocktailClasses: []}) {
+  return new SchemePunkMolotov(configObject.overrides, configObject.cocktailClasses);
+};
