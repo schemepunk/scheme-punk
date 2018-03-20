4e6181a4528239b15e8fe8ba21079cccd9908009
David Diers <david.diers@fourkitchens.com>
2018-03-19 21:09:53 -0500

    feat(schemeRunner): Added a schemeRunner to reduce duplication in schemePunk implementers.

---
+++ __tests__/lib/SchemeRunner.js
@@ -0,0 +1,124 @@
+'use strict';
+
+const SchemeRunner = require('../../lib/SchemeRunner');
+const SchemePunkErrors = require('../../lib/SchemePunkErrors');
+const _ = require('lodash');
+
+let tmpMocks = [];
+let data;
+let schemeArray;
+let molotovOptions;
+
+beforeEach(() => {
+  tmpMocks.forEach(mock => mock.mockRestore());
+  tmpMocks = [];
+  jest.resetAllMocks();
+  jest.spyOn(Date, 'now').mockReturnValue(2000);
+  data = {test: 'testValue'};
+  schemeArray = {
+    arbitraryNameForThisScheme: [
+      [
+        {
+          source: {
+            target: 'test',
+            plugin: 'originalSchemeSource'
+          },
+          transform: {
+            plugin: 'prependValuesAdapter',
+            sourcePrepend: '+'
+          },
+          destination: {
+            target: 'test'
+          }
+        }
+      ],
+      [
+        {
+          source: {
+            target: 'test',
+            plugin: 'originalSchemeSource'
+          },
+          transform: {
+            plugin: 'appendValuesAdapter',
+            sourceAppend: '-'
+          },
+          destination: {
+            target: 'test',
+            plugin: 'concatIntoDestination'
+          }
+        }
+      ]
+    ],
+  };
+  molotovOptions = {
+    overrides: {},
+    cocktailClasses: []
+  };
+});
+
+afterAll(() => {
+  jest.restoreAllMocks();
+});
+
+describe('Scheme Runner', () => {
+  test('Basic init and scheme running', () => {
+    expect.assertions(5);
+    const schemeRunner = new SchemeRunner();
+    expect(schemeRunner).toBeInstanceOf(SchemeRunner);
+    return schemeRunner.init(_.cloneDeep(data), _.cloneDeep(schemeArray), _.cloneDeep(molotovOptions))
+      .then((sr) => {
+        expect(sr.getData()).toMatchSnapshot();
+        expect(sr.getSchemes()).toMatchSnapshot();
+        expect(sr.getMolotovOptions()).toMatchSnapshot();
+        return schemeRunner.runScheme();
+      })
+      .then(item => expect(item).toEqual({
+        test: [
+          '+testValue',
+          'testValue-'
+        ]
+      }));
+  });
+});
+
+describe('Scheme Runner Error Cases', () => {
+  test('No data', () => {
+    expect.assertions(1);
+    const schemeRunner = new SchemeRunner();
+    return schemeRunner.init(undefined, _.cloneDeep(schemeArray), _.cloneDeep(molotovOptions))
+      .catch(e => expect(e).toBeInstanceOf(SchemePunkErrors));
+  });
+  test('No Scheme', () => {
+    expect.assertions(1);
+    const schemeRunner = new SchemeRunner();
+    return schemeRunner.init(_.cloneDeep(data), undefined, _.cloneDeep(molotovOptions))
+      .catch(e => expect(e).toBeInstanceOf(SchemePunkErrors));
+  });
+});
+
+
+describe('Scheme Runner Coverage', () => {
+  test('Basic init and scheme running', () => {
+    expect.assertions(5);
+    const schemeRunner = new SchemeRunner();
+    expect(schemeRunner).toBeInstanceOf(SchemeRunner);
+    const tmpData = {
+      originalScheme: _.cloneDeep(data),
+      activeScheme: _.cloneDeep(data),
+      newScheme: _.cloneDeep(data)
+    };
+    return schemeRunner.init(tmpData, _.cloneDeep(schemeArray), _.cloneDeep(molotovOptions))
+      .then((sr) => {
+        expect(sr.getData()).toMatchSnapshot();
+        expect(sr.getSchemes()).toMatchSnapshot();
+        expect(sr.getMolotovOptions()).toMatchSnapshot();
+        return schemeRunner.runScheme();
+      })
+      .then(item => expect(item).toEqual({
+        test: [
+          '+testValue',
+          'testValue-'
+        ]
+      }));
+  });
+});
