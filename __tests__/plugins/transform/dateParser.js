ad9d4715af8bcc4bf9741685482538514de0fbaa
David Diers <david.diers@fourkitchens.com>
2019-08-20 13:11:35 -0500

    fix(tests): fixing tests for mocked time

--- __tests__/plugins/transform/dateParser.js
+++ __tests__/plugins/transform/dateParser.js
@@ -9,7 +9,7 @@ class BaseXform {
   }
 
   async transform(value) { // eslint-disable-line class-methods-use-this
-    return value.format();
+    return value.format('YYYY-MM-DD');
   }
 
   async getHoldOvers() {
@@ -34,7 +34,7 @@ describe('Date parse', async () => {
   test('string can be parsed', async () => {
     // Test case value.
     value = '2013-02-08';
-    expect(await dateParse.transform(value)).toBe('2013-02-08T00:00:00-06:00');
+    expect(await dateParse.transform(value)).toBe('2013-02-08');
   });
 
   test('string with inputFormat can be parsed', async () => {
@@ -44,7 +44,7 @@ describe('Date parse', async () => {
     };
     // Test case value.
     value = '2013-02-08';
-    expect(await dateParse.transform(value)).toEqual('2013-02-08T00:00:00-06:00');
+    expect(await dateParse.transform(value)).toEqual('2013-02-08');
   });
 
   test('string with bad inputFormat errors', async () => {
@@ -66,7 +66,7 @@ describe('Date parse', async () => {
     // Test case value.
     _.unset(dateParse, ['options']);
     value = [2013, 2, 8, 0, 0, 0, 0];
-    expect(await dateParse.transform(value)).toEqual('2013-02-08T00:00:00-06:00');
+    expect(await dateParse.transform(value)).toEqual('2013-02-08');
   });
 
   test('other formats with spread are cool object', async () => {
@@ -74,16 +74,16 @@ describe('Date parse', async () => {
     value = {
       years: '2013', months: '2', date: '8', hours: '0', minutes: '0', seconds: '0', milliseconds: '000'
     };
-    expect(await dateParse.transform(value)).toEqual('2013-02-08T00:00:00-06:00');
+    expect(await dateParse.transform(value)).toEqual('2013-02-08');
   });
 
   test('nothing provided gives now.', async () => {
     value = '';
-    expect(await dateParse.transform(value)).toEqual('2013-02-08T00:00:00-06:00');
+    expect(await dateParse.transform(value)).toEqual('2013-02-08');
   });
 
   test('undefined provided gives now.', async () => {
-    expect(await dateParse.transform()).toEqual('2013-02-08T00:00:00-06:00');
+    expect(await dateParse.transform()).toEqual('2013-02-08');
   });
 
   test('problems.', async () => {
@@ -105,6 +105,6 @@ describe('Date parse', async () => {
       inputUseUtc: true
     };
     value = '2013-08-02';
-    expect(await dateParse.transform()).toEqual('2013-02-08T06:00:00Z');
+    expect(await dateParse.transform()).toEqual('2013-02-08');
   });
 });
